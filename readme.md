5b38bd9a6dfe08ede833337dc6c88a93bdf7cd35
Mark Harmon <neothej1sec@gmail.com>
2023-02-16 01:05:16 -0600

    started working with swift

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Basic Notes:
+
+#### Basics
+![s](./assets/s2.png)
+#### Math
+![s](./assets/s1.png)
+#### Trig Math Funcs
+![s](./assets/s3.png)
+#### Conditionals
+![s](./assets/s.png)
+
+
---
+++ swiftTut/swiftTut/swiftTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftTutApp.swift
+//  swiftTut
+//
+//  Created by Mark Harmon on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
