c2c5abd70437f2e992e50d3e22e2d0b9e6c9566c
Mark Harmon <neothej1sec@gmail.com>
2023-02-16 14:52:07 -0600

    working with functions recursion etc

--- swiftTutPlayground.playground/Contents.swift
+++ swiftTutPlayground.playground/Contents.swift
@@ -1,2 +1,29 @@
 import Cocoa
 
+//assign func to func
+func multiplyBy2(_ num: Int) -> Int{
+    return num * 2
+}
+var timesTwo = multiplyBy2
+print("4 * 2 = \(timesTwo(4))")
+// asign func to run func
+func runFunc(_ f:() -> ()){
+    f()
+}
+func print4() -> Void{
+    print("4")
+}
+runFunc(print4)
+
+func doMath(_ f:(Int) -> Int, _ x: Int){
+    print("Result: \(f(x))")
+}
+doMath(timesTwo, 4)
+func funcMaker(val: Int) -> (Int) -> Int{
+    func addVals(num1: Int) -> Int{
+        return num1 + val
+    }
+    return addVals
+}
+let add4 = funcMaker(val: 4)
+print("4 + 5 \(add4(5))")
